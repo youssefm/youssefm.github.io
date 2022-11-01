fc5092c04b1bd2ef91ee3199e451129add1564ca
Youssef Moussaoui <yms@outlook.com>
2022-10-31 21:34:22 -0700

    Remove light mode support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
