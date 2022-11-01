86e466cbdfed5e9bd82d5fc4d99dc4b09e3e2bc1
Youssef Moussaoui <yms@outlook.com>
2022-11-01 12:14:48 -0700

    Improve header layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   markdown: {
-    rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: [] }]],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: [],
+        },
+      ],
+    ],
     extendDefaultPlugins: true,
   },
   site: "https://youssefm.github.io",
