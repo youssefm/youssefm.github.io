980fd5934bd70cf95fcd35a90cd1e45550b7a4d2
Youssef Moussaoui <yms@outlook.com>
2022-11-01 22:20:18 -0700

    Add published dates to blog posts

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -24,7 +24,7 @@ export const sortBy = <T>(
 export const formatPublishedOn = (publishedOn: string) => {
   const publishedOnDate = new Date(publishedOn);
   return publishedOnDate.toLocaleDateString("en-US", {
-    month: "long",
-    year: "numeric",
+    dateStyle: "long",
+    timeZone: "UTC",
   });
 };
